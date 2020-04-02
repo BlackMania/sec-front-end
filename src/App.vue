98005710a7bd3aba566e26ee74fb985917972ce3
Vincent <vincentmuijtjens@ziggo.nl>
2020-04-02 11:07:08 +0200

    Added login stuff

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="app">
+    <router-view />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+
+    };
 </script>
 
-<style>
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #CFD8DC;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container fill-height >
+        <v-layout class="align-center">
+            <v-card width="100%" color="blue-grey lighten-5" class="text-center">
+                <v-card-title>
+                    Login using Facial Recognition
+                </v-card-title>
+            </v-card>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+</style>
