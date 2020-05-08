f62e7ccb5d16c6b3245db9a40ffb820b2184f992
Vincent <vincentmuijtjens@ziggo.nl>
2020-05-08 09:12:53 +0200

    Camera can now be used by program

---
+++ src/views/FacialRecognitionLogin.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container fill-height class="d-flex justiy-center">
+    <video
+      class="video d-flex justify-center"
+      id="video"
+      style="margin: auto"
+      width="800"
+      height="auto"
+      autoplay="true"
+      ref="video"
+    ></video>
+  </v-container>
+</template>
+
+<script>
+import * as faceapi from 'face-api.js';
+export default {
+  name: "FacialRecognitionLogin",
+  async mounted() {
+    const video = document.getElementById('video');
+
+    await Promise.all([
+      faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+      faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+      faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+      faceapi.nets.faceExpressionNet.loadFromUri("/models")
+    ]).then(startVideo)
+
+    function startVideo() {
+      navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+      )
+    }
+
+    video.addEventListener('play', () => {
+      const canvas = faceapi.createCanvasFromMedia(video)
+      document.body.append(canvas)
+      const displaySize = { width: video.width, height: video.height }
+      faceapi.matchDimensions(canvas, displaySize)
+      setInterval(async () => {
+        const detections = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+        console.log(detections);
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+      }, 100)
+    })
+  }
+};
+</script>
+
+<style scoped>
+.video {
+  border-radius: 100%;
+}
+</style>
