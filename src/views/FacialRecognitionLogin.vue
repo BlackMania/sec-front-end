49d9d5f1ce76462c0d9ca5d61c184003e2b882f5
BlackMania <BlackMania@users.noreply.github.com>
2020-05-08 10:41:17 +0200

    Fixed facial detection with models

--- src/views/FacialRecognitionLogin.vue
+++ src/views/FacialRecognitionLogin.vue
@@ -1,60 +1,64 @@
 <template>
   <v-container fill-height class="d-flex justiy-center">
-    <video
-      class="video d-flex justify-center"
-      id="video"
-      style="margin: auto"
-      width="800"
-      height="auto"
-      autoplay="true"
-      ref="video"
-    ></video>
+    <div id="container">
+      <video id="video" width="720" height="560" autoplay muted></video>
+    </div>
   </v-container>
 </template>
 
 <script>
-import * as faceapi from 'face-api.js';
+import * as faceapi from "face-api.js";
+
 export default {
   name: "FacialRecognitionLogin",
   async mounted() {
-    const video = document.getElementById('video');
+    const video = document.getElementById("video");
 
-    await Promise.all([
+    Promise.all([
       faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
       faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
       faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
       faceapi.nets.faceExpressionNet.loadFromUri("/models")
-    ]).then(startVideo)
+    ]).then(startVideo);
 
     function startVideo() {
       navigator.getUserMedia(
         { video: {} },
-        stream => video.srcObject = stream,
+        stream => (video.srcObject = stream),
         err => console.error(err)
-      )
+      );
     }
 
-    video.addEventListener('play', () => {
-      const canvas = faceapi.createCanvasFromMedia(video)
-      document.body.append(canvas)
-      const displaySize = { width: video.width, height: video.height }
-      faceapi.matchDimensions(canvas, displaySize)
+    video.addEventListener("play", () => {
+      const canvas = faceapi.createCanvasFromMedia(video);
+      const container = document.getElementById("container");
+      canvas.style.position = "absolute";
+      canvas.style.top = 0;
+      canvas.style.left = 0;
+
+      container.append(canvas);
+      const displaySize = { width: video.width, height: video.height };
+      faceapi.matchDimensions(canvas, displaySize);
       setInterval(async () => {
-        const detections = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
-        console.log(detections);
-        const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        faceapi.draw.drawDetections(canvas, resizedDetections)
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-      }, 100)
-    })
+        const detections = await faceapi
+          .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
+          .withFaceLandmarks();
+        const resizedDetections = faceapi.resizeResults(
+          detections,
+          displaySize
+        );
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      }, 5);
+    });
   }
 };
 </script>
 
 <style scoped>
-.video {
-  border-radius: 100%;
+#container {
+  margin: auto;
+  position: relative;
 }
 </style>
