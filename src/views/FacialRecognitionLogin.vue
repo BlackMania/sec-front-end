182296852e7ead1767cb020b1384af2ad938d9aa
BlackMania <BlackMania@users.noreply.github.com>
2020-06-15 14:52:12 +0200

    Connected to backend

--- src/views/FacialRecognitionLogin.vue
+++ src/views/FacialRecognitionLogin.vue
@@ -8,10 +8,22 @@
 
 <script>
 import * as faceapi from "face-api.js";
+import axios from 'axios';
 
 export default {
   name: "FacialRecognitionLogin",
+  data() {
+    return {
+      api: null
+    }
+  },
+  created() {
+    this.api = axios.create({
+      url: 'http://172.16.1.2:8080'
+    })
+  },
   async mounted() {
+    const self = this;
     const video = document.getElementById("video");
 
     Promise.all([
@@ -52,6 +64,44 @@ export default {
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
       }, 5);
     });
+
+    setTimeout(function() {
+      const imgCanvas = document.createElement("canvas");
+      imgCanvas.width = video.videoWidth;
+      imgCanvas.height = video.videoHeight;
+      imgCanvas.getContext('2d').drawImage(video, 0, 0, imgCanvas.width, imgCanvas.height);
+      const dataURL = imgCanvas.toDataURL('image/jpeg', 0.5);
+      window.console.log(self.dataURItoBlob(dataURL))
+      var fd = new FormData(document.forms[0]);
+      fd.append("file", self.dataURItoBlob(dataURL));
+      self.api.post("http://172.16.1.2:8080/auth/login", fd, {
+        headers: {
+          'Content-Type': "multipart/form-data"
+        }
+      })
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }, 10000)
+  },
+  methods: {
+    dataURItoBlob: function(dataURI) {
+        var byteString;
+        if (dataURI.split(',')[0].indexOf('base64') >= 0)
+            byteString = atob(dataURI.split(',')[1]);
+        else
+            byteString = unescape(dataURI.split(',')[1]);
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        var ia = new Uint8Array(byteString.length);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        return new Blob([ia], {type:mimeString});
+    }
   }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,6 @@ const fs = require('fs')
 
 module.exports = {
     devServer: {
-        https: {
-          key: fs.readFileSync('./certs/private.key'),
-          cert: fs.readFileSync('./certs/certificate.crt'),
-        },
-        public: 'https://localhost:8080/'
+        public: 'http://localhost:8080/'
     }
 }
